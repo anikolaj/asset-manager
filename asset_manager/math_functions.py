f0034ce8aaec1edaad3fa20171251c8da5599065
Andy Nikolajuk <anikolaj@umich.edu>
2020-12-07 11:22:37 -0600

    adding check for returs array length when computing correlation coefficient

--- asset_manager/math_functions.py
+++ asset_manager/math_functions.py
@@ -1,9 +1,17 @@
 from scipy.stats import pearsonr
 
 def compute_covariance_with_correlation_coefficient(x_data, y_data, x_stdev, y_stdev):
-	print("X length - " + str(len(x_data)))
-	print("Y length - " + str(len(y_data)))
+	x_len = len(x_data)
+	y_len = len(y_data)
+
+	if x_len != y_len:
+		diff = abs(x_len - y_len)
+		if x_len > y_len:
+			x_data = x_data[diff : x_len]
+		else:
+			y_data = y_data[diff : y_len]
 	
 	rho, p_val = pearsonr(x_data, y_data)
 	covariance = rho * x_stdev * y_stdev
+	
 	return covariance
