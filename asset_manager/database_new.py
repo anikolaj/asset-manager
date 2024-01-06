a9a8e598b5fb2959d866bfe12f3f120a8320ff14
anikolaj <anikolaj@umich.edu>
2024-01-06 00:28:54 -0500

    - updated database_new module to to utilize black, flake8, and mypy

--- asset_manager/database_new.py
+++ asset_manager/database_new.py
@@ -1,6 +1,7 @@
-from datetime import date
 from bson import ObjectId
+from datetime import date
 from pymongo import MongoClient
+from typing import Optional
 
 from asset_manager.entities_new import Equity, Lot, Portfolio, Trade, Valuation
 
@@ -8,7 +9,7 @@ from asset_manager.entities_new import Equity, Lot, Portfolio, Trade, Valuation
 class Database:
     def __init__(self, user: str, password: str, database: str, cluster: str) -> None:
         self.connection = f"mongodb+srv://{user}:{password}@{cluster}/{database}?retryWrites=true&w=majority"
-        self.client = MongoClient(host=self.connection)
+        self.client: MongoClient = MongoClient(host=self.connection)
         self.db = self.client.get_database(database)
 
     def get_portfolio_names(self) -> list[str]:
@@ -21,7 +22,16 @@ class Database:
         portfolios = self.db.get_collection("portfolio")
         return [p["name"] for p in portfolios.find()]
 
-    def get_portfolio_by_name(self, portfolio_name: str) -> Portfolio:
+    def get_portfolio_by_name(self, portfolio_name: str) -> Optional[Portfolio]:
+        """Returns portfolio object stored in database
+
+        Args:
+            portfolio_name (str): name of the portfolio
+
+        Returns:
+            Optional[Portfolio]: portfolio object, returns None if no such portfolio exists
+        """
+
         portfolios = self.db.get_collection("portfolio")
         portfolio_entity = portfolios.find_one({"name": portfolio_name})
 
@@ -45,10 +55,19 @@ class Database:
             cash=portfolio_entity["cash"],
             equities=equities,
             trades=trades,
-            valuation=valuation
+            valuation=valuation,
         )
 
     def create_portfolio(self, portfolio_name: str) -> Portfolio:
+        """Creates a new portfolio with the provided name
+
+        Args:
+            portfolio_name (str): name to be used for the portfolio
+
+        Returns:
+            Portfolio: portfolio object
+        """
+
         portfolio = Portfolio(
             id=ObjectId(),
             name=portfolio_name,
@@ -62,8 +81,8 @@ class Database:
                 pnl=0,
                 realized_pnl=0,
                 year_start_value=0,
-                current_year=date.today().year
-            )
+                current_year=date.today().year,
+            ),
         )
 
         portfolios = self.db.get_collection("portfolio")
@@ -72,17 +91,14 @@ class Database:
         return portfolio
 
     def save_portfolio(self, portfolio: Portfolio) -> None:
+        """Saves provided portfolio to the database
+
+        Args:
+            portfolio (Portfolio): portfolio object
+        """
+
         portfolios = self.db.get_collection("portfolio")
         portfolios.update_one({"_id": portfolio.id}, {"$set": portfolio.to_dict()})
-
-    def add_equity_to_portfolio(self, portfolio: Portfolio, new_equity: Equity) -> None:
-        portfolio.equities.append(new_equity)
-        portfolios = self.db.get_collection("portfolio")
-        portfolios.update_one({"_id": portfolio.id}, {"$push": {"equities": new_equity.to_dict()}})
-
-    def save_trade(self, trade: Trade) -> None:
-        trades = self.db.get_collection("trades")
-        trades.insert_one(trade.to_dict())
 
     def __convert_entity_to_equity(self, equity_entity: dict) -> Equity:
         return Equity(
@@ -93,7 +109,7 @@ class Database:
             year_start_price=equity_entity["yearStartPrice"],
             ytd=equity_entity["ytd"],
             previous_day_price=equity_entity["previousDayPrice"],
-            lots=[self.__convert_entity_to_lot(lot) for lot in equity_entity["lots"]]
+            lots=[self.__convert_entity_to_lot(lot) for lot in equity_entity["lots"]],
         )
 
     def __convert_entity_to_trade(self, trade_entity: dict) -> Trade:
@@ -101,7 +117,7 @@ class Database:
             ticker=trade_entity["ticker"],
             price=trade_entity["price"],
             shares=trade_entity["shares"],
-            execution_time=trade_entity["executionTime"]
+            execution_time=trade_entity["executionTime"],
         )
 
     def __convert_entity_to_valuation(self, valuation_entity: dict) -> Valuation:
@@ -111,12 +127,12 @@ class Database:
             pnl=valuation_entity["pnl"],
             realized_pnl=valuation_entity["realizedPnl"],
             year_start_value=valuation_entity["yearStartValue"],
-            current_year=valuation_entity["currentYear"]
+            current_year=valuation_entity["currentYear"],
         )
 
     def __convert_entity_to_lot(self, lot_entity: dict) -> Lot:
         return Lot(
             shares=lot_entity["shares"],
             price=lot_entity["price"],
-            execution_time=lot_entity["executionTime"]
+            execution_time=lot_entity["executionTime"],
         )
