5ce5838d053d67769020e9cf4cdbf2b893b42501
anikolaj <anikolaj@umich.edu>
2024-01-06 01:04:56 -0500

    - updated entities_new module to to utilize black, flake8, and mypy

--- asset_manager/entities_new.py
+++ asset_manager/entities_new.py
@@ -13,7 +13,7 @@ class Lot:
         return {
             "shares": self.shares,
             "price": self.price,
-            "executionTime": self.execution_time
+            "executionTime": self.execution_time,
         }
 
     def __repr__(self) -> str:
@@ -30,7 +30,7 @@ class Equity:
         lots: list[Lot],
         weight: float = 0,
         year_start_price: float = 0,
-        ytd: float = 0
+        ytd: float = 0,
     ) -> None:
         self.ticker = ticker
         self.shares = shares
@@ -50,14 +50,16 @@ class Equity:
             "previousDayPrice": self.previous_day_price,
             "yearStartPrice": self.year_start_price,
             "ytd": self.ytd,
-            "lots": [lot.to_dict() for lot in self.lots]
+            "lots": [lot.to_dict() for lot in self.lots],
         }
 
     def __repr__(self) -> str:
-        return f"Equity(ticker={self.ticker}, shares={self.shares}, " \
-               f"weight={self.weight}, price={self.price}, previous_day_price={self.previous_day_price}, " \
-               f"year_start_price={self.year_start_price}, ytd={self.ytd}, " \
-               f"lots={'[..]' if len(self.lots) != 0 else '[]'})"
+        return (
+            f"Equity(ticker={self.ticker}, shares={self.shares}, "
+            f"weight={self.weight}, price={self.price}, previous_day_price={self.previous_day_price}, "
+            f"year_start_price={self.year_start_price}, ytd={self.ytd}, "
+            f"lots={'[..]' if len(self.lots) != 0 else '[]'})"
+        )
 
 
 class Valuation:
@@ -68,7 +70,7 @@ class Valuation:
         pnl: float,
         realized_pnl: float,
         year_start_value: float,
-        current_year: datetime
+        current_year: int,
     ) -> None:
         self.current_value = current_value
         self.ytd = ytd
@@ -84,22 +86,20 @@ class Valuation:
             "pnl": self.pnl,
             "realizedPnl": self.realized_pnl,
             "yearStartValue": self.year_start_value,
-            "currentYear": self.current_year
+            "currentYear": self.current_year,
         }
 
     def __repr__(self) -> str:
-        return f"Valuation(current_value={self.current_value}, ytd={self.ytd}, " \
-               f"pnl={self.pnl}, realized_pnl={self.realized_pnl}, year_start_value={self.year_start_value}, " \
-               f"current_year={self.current_year})"
+        return (
+            f"Valuation(current_value={self.current_value}, ytd={self.ytd}, "
+            f"pnl={self.pnl}, realized_pnl={self.realized_pnl}, year_start_value={self.year_start_value}, "
+            f"current_year={self.current_year})"
+        )
 
 
 class Trade:
     def __init__(
-        self,
-        ticker: str,
-        price: float,
-        shares: float,
-        execution_time: datetime
+        self, ticker: str, price: float, shares: float, execution_time: datetime
     ) -> None:
         self.ticker = ticker
         self.price = price
@@ -111,11 +111,14 @@ class Trade:
             "ticker": self.ticker,
             "price": self.price,
             "shares": self.shares,
-            "executionTime": self.execution_time
+            "executionTime": self.execution_time,
         }
 
     def __repr__(self) -> str:
-        return f"Trade(ticker={self.ticker}, price={self.price}, shares={self.shares}, execution_time={self.execution_time})"
+        return (
+            f"Trade(ticker={self.ticker}, price={self.price}, "
+            f"shares={self.shares}, execution_time={self.execution_time})"
+        )
 
 
 class Portfolio:
@@ -127,7 +130,7 @@ class Portfolio:
         cash: float,
         equities: list[Equity],
         trades: list[Trade],
-        valuation: Valuation
+        valuation: Valuation,
     ) -> None:
         self.id = id
         self.name = name
@@ -145,10 +148,13 @@ class Portfolio:
             "cash": self.cash,
             "equities": [equity.to_dict() for equity in self.equities],
             "trades": [trade.to_dict() for trade in self.trades],
-            "valuation": self.valuation.to_dict()
+            "valuation": self.valuation.to_dict(),
         }
 
     def __repr__(self) -> str:
-        return f"Portfolio(id={self.id}, name={self.name}, value={self.value}, cash={self.cash}, " \
-               f"equities={'[..]' if len(self.equities) != 0 else '[]'}, trades={'[..]' if len(self.equities) != 0 else '[]'}), " \
-               f"valuation={self.valuation!r}"
+        return (
+            f"Portfolio(id={self.id}, name={self.name}, value={self.value}, cash={self.cash}, "
+            f"equities={'[..]' if len(self.equities) != 0 else '[]'}, "
+            f"trades={'[..]' if len(self.equities) != 0 else '[]'}), "
+            f"valuation={self.valuation!r}"
+        )
