1bf5d48fd9c6daf9cda6fd075f977379618af5b5
anikolaj <anikolaj@umich.edu>
2024-01-08 01:30:21 -0500

    - updated equity_interface/helpers module to to utilize black, flake8, and mypy

--- asset_manager/equity_interface/helpers.py
+++ asset_manager/equity_interface/helpers.py
@@ -1,11 +1,22 @@
-import statistics
+from statistics import stdev
 from scipy.stats import gmean
 
 from asset_manager.objects import Interval, TimeSeriesDetails
 
 
-# method will parse out the relevant time series array from the monthly stock prices
-def parse_prices_for_time_interval(monthly_prices: list, time_interval: Interval) -> list:
+def parse_prices_for_time_interval(
+    monthly_prices: list, time_interval: Interval
+) -> list:
+    """Method will parse out the relevant time series array from the monthly stock prices
+
+    Args:
+        monthly_prices (list): list of monthly prices for the stock
+        time_interval (Interval): time period to split prices
+
+    Returns:
+        list: list of prices representing the time interval
+    """
+
     if time_interval == Interval.MONTH:
         return monthly_prices
 
@@ -26,16 +37,24 @@ def parse_prices_for_time_interval(monthly_prices: list, time_interval: Interval
     return monthly_prices[::split_value]
 
 
-# method will calculate the returns array, average return, and risk
 def calculate_time_series_details(time_series: list) -> TimeSeriesDetails:
+    """Method will calculate the returns array, average return, and risk
+
+    Args:
+        time_series (list): list of prices for the time interval
+
+    Returns:
+        TimeSeriesDetails: object containing statistics for the time series
+    """
+
     total_samples = len(time_series)
 
     percent_changes = []
     returns = []
 
-    for i in range(total_samples-1, 0, -1):
+    for i in range(total_samples - 1, 0, -1):
         present_price = time_series[i]
-        previous_price = time_series[i-1]
+        previous_price = time_series[i - 1]
 
         percent_change = present_price / previous_price
         rate_of_return = percent_change - 1
@@ -43,8 +62,4 @@ def calculate_time_series_details(time_series: list) -> TimeSeriesDetails:
         percent_changes.append(percent_change)
         returns.append(rate_of_return)
 
-    stdev = None
-    if len(returns) > 1:
-        stdev = statistics.stdev(returns)
-
-    return TimeSeriesDetails(returns, gmean(percent_changes)-1, stdev)
+    return TimeSeriesDetails(returns, gmean(percent_changes) - 1, stdev(returns))
