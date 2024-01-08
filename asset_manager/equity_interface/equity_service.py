ba48a969fc53b0991011f1d26ce4400e813c5bee
anikolaj <anikolaj@umich.edu>
2024-01-08 01:45:46 -0500

    - updated equity_interface/equity_service module to to utilize black, flake8, and mypy

--- asset_manager/equity_interface/equity_service.py
+++ asset_manager/equity_interface/equity_service.py
@@ -7,15 +7,39 @@ from asset_manager.objects import Interval, TimeSeriesDetails
 class EquityService(ABC):
     @abstractmethod
     def get_equity_prices(self, ticker: str) -> tuple[float, float]:
-        # method retrieves the most recent price for the equity
+        """Method retrieves the current price and previous day price for the equity
+
+        Args:
+            ticker (str): stock ticker
+
+        Returns:
+            tuple[float, float]: current day price, previous day price
+        """
         pass
 
     @abstractmethod
     def get_equity_year_start_price(self, ticker: str) -> float:
-        # method retrieves the year start price for the equity
+        """Method retrieves the year start price for the equity
+
+        Args:
+            ticker (str): stock ticker
+
+        Returns:
+            float: year start price
+        """
         pass
 
     @abstractmethod
-    def update_equity_details(self, eq: Equity, time_interval: Interval) -> TimeSeriesDetails:
-        # method computes average daily return for the equity
+    def update_equity_details(
+        self, eq: Equity, time_interval: Interval
+    ) -> TimeSeriesDetails:
+        """Method computes returns, average return, and standard deviation for the equity over the provided interval
+
+        Args:
+            eq (Equity): object for equity in portfolio
+            time_interval (Interval): time period to calculate returns and statistics
+
+        Returns:
+            TimeSeriesDetails: object containing statistics for the equity over the interval
+        """
         pass
