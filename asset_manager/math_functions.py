3ab26be1cdd4551114ef40d957d24326b655b0a2
anikolaj <anikolaj@umich.edu>
2024-01-13 01:43:26 -0500

    - updated math_functions module to to utilize black, flake8, and mypy

--- asset_manager/math_functions.py
+++ asset_manager/math_functions.py
@@ -1,52 +1,55 @@
 import math
 import numpy as np
-
 from scipy.stats import pearsonr
+from typing import Optional
 
 
-def compute_covariance_with_correlation_coefficient(x_data: list[float], y_data: list[float], x_stdev: float, y_stdev: float) -> np.float64:
-	x_len = len(x_data)
-	y_len = len(y_data)
+def compute_covariance_with_correlation_coefficient(
+    x_data: list[float],
+    y_data: list[float],
+    x_stdev: float,
+    y_stdev: float,
+) -> np.float64:
+    x_len = len(x_data)
+    y_len = len(y_data)
 
-	if x_len != y_len:
-		diff = abs(x_len - y_len)
-		if x_len > y_len:
-			x_data = x_data[diff : x_len]
-		else:
-			y_data = y_data[diff : y_len]
+    if x_len != y_len:
+        diff = abs(x_len - y_len)
+        if x_len > y_len:
+            x_data = x_data[diff:x_len]
+        else:
+            y_data = y_data[diff:y_len]
 
-	covariance = None
-	if len(x_data) > 1:
-		rho, p_val = pearsonr(x_data, y_data)
-		covariance = rho * x_stdev * y_stdev
+    rho, p_val = pearsonr(x_data, y_data)
+    covariance = rho * x_stdev * y_stdev
 
-	return covariance
+    return covariance
 
 
 def calculate_expected_value(X: np.ndarray, p: np.ndarray) -> float:
-	p_t = np.transpose(p)
-	expected_value = round(X @ p_t, 8)
-	return float(expected_value)
+    p_t = np.transpose(p)
+    expected_value = round(X @ p_t, 8)
+    return float(expected_value)
 
 
 def calculate_variance(C: np.ndarray, p: np.ndarray) -> float:
-	p_t = np.transpose(p)
+    p_t = np.transpose(p)
 
-	variance = None
-	if C.size != 0:
-		variance = p @ C @ p_t
-		variance = round(variance.item(), 8)
+    variance = p @ C @ p_t
+    variance = round(variance.item(), 8)
 
-	return variance
+    return variance
 
 
-def solve_quadratic_formula(a: np.float64, b: np.float64, c: np.float64) -> tuple[np.float64, np.float64]:
-	r = (b ** 2) - (4 * a * c)
+def solve_quadratic_formula(
+    a: np.float64, b: np.float64, c: np.float64
+) -> Optional[tuple[np.float64, np.float64]]:
+    r = (b**2) - (4 * a * c)
 
-	if r == -1:
-		return None
+    if r == -1:
+        return None
 
-	x1 = (-b + math.sqrt(r)) / (2 * a)
-	x2 = (-b - math.sqrt(r)) / (2 * a)
+    x1 = (-b + math.sqrt(r)) / (2 * a)
+    x2 = (-b - math.sqrt(r)) / (2 * a)
 
-	return x1, x2
+    return x1, x2
