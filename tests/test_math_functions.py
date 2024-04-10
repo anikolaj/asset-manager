f76bee366bf090804644d44615134233a2c9cf63
anikolaj <anikolaj@umich.edu>
2024-04-10 17:54:26 -0400

    - updated math_functions tests

--- tests/test_math_functions.py
+++ tests/test_math_functions.py
@@ -1,13 +1,12 @@
-import unittest
-import asset_manager.math_functions as mf
+import asset_manager.utilities.math_functions as mf
 
-class TestMathFunctions(unittest.TestCase):
-	def test_compute_covariance_with_correlation_coefficient(self):
-		x_data = [1, 2, 3]
-		y_data = [1, 2, 3]
-		x_stdev = 2
-		y_stdev = 4
 
-		result = mf.compute_covariance_with_correlation_coefficient(x_data, y_data, x_stdev, y_stdev)
-		rounded_result = round(result, 3)
-		self.assertEqual(rounded_result, 8.0)
+def test_compute_covariance_with_correlation_coefficient():
+    x_data = [1, 2, 3]
+    y_data = [1, 2, 3]
+    x_stdev = 2
+    y_stdev = 4
+
+    result = mf.compute_covariance_with_correlation_coefficient(x_data, y_data, x_stdev, y_stdev)
+    rounded_result = round(result, 3)
+    assert rounded_result == 8.0
